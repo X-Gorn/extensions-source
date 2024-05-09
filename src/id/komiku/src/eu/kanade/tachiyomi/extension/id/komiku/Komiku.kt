7d08d3a0028ebb3b5a019cc11f53267f43f005a2
Noid <xgorntv@gmail.com>
2024-05-09 10:52:23 +0900

    test: trigger worflow

--- src/id/komiku/src/eu/kanade/tachiyomi/extension/id/komiku/Komiku.kt
+++ src/id/komiku/src/eu/kanade/tachiyomi/extension/id/komiku/Komiku.kt
@@ -29,7 +29,7 @@ class Komiku : ParsedHttpSource() {
 
     override val client: OkHttpClient = network.cloudflareClient
 
-    // popular
+    // popular selector
     override fun popularMangaSelector() = "div.bge"
 
     override fun popularMangaRequest(page: Int): Request {
