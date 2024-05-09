f56ecf55688cf35cb365142db5d30eddd23ff494
Noid <xgorntv@gmail.com>
2024-05-09 11:40:26 +0900

    test: re-trigger workflow.

--- src/id/komiku/src/eu/kanade/tachiyomi/extension/id/komiku/Komiku.kt
+++ src/id/komiku/src/eu/kanade/tachiyomi/extension/id/komiku/Komiku.kt
@@ -29,7 +29,7 @@ class Komiku : ParsedHttpSource() {
 
     override val client: OkHttpClient = network.cloudflareClient
 
-    // popular selector
+    // popular selector (2)
     override fun popularMangaSelector() = "div.bge"
 
     override fun popularMangaRequest(page: Int): Request {
